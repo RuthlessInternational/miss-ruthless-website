ad9824bfae47a58b7866e6721d17a1f7d5120da2
Callil Capuozzo <callil.capuozzo@gmail.com>
2018-07-15 01:49:27 -0400

    Design changes

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import Prismic from 'prismic-javascript';
+
+import './Header.css';
+
+export default class Header extends React.Component {
+    state = {
+        backText_english: "",
+        backText_chinese: ""
+    }
+
+    addBack = () =>{
+        console.log("ok?")
+        {this.props.navTo ? 
+            this.setState({backText_english: "Back", backText_chinese: "回"})
+                :
+            this.setState({backText_english: "Close", backText_chinese: "关"})
+        }
+    }
+
+    removeBack = () =>{
+        console.log("yes?")
+        this.setState({backText_english: "", backText_chinese: ""})
+    }
+
+    render (){
+        return (    
+            <section className="header">
+                {this.props.navTo ? 
+                    <Link to={"/"} onMouseEnter={(e)=> this.addBack()} onMouseLeave={(e)=> this.removeBack()} className="arrow">
+                        <img src={process.env.PUBLIC_URL + "/images/arrow.png"} />
+                        <h1 className="backtext"><span className="mincho">{this.state.backText_chinese}</span>{this.state.backText_english}</h1>
+                        </Link>
+                :
+                    <Link to={"/" + this.props.context + "/"} onMouseEnter={(e)=> this.addBack()} onMouseLeave={(e)=> this.removeBack()} className="arrow"><img src={process.env.PUBLIC_URL + "/images/close.png"} /><h1 className="backtext"><span className="mincho">{this.state.backText_chinese}</span>{this.state.backText_english}</h1></Link>
+                }
+                <h1><span className="mincho">{this.props.title_chinese}</span>{this.props.title_english}</h1>
+                <p></p>
+            </section>
+        )
+    }
+}
